eadb0041996fd7e8f6f42767cfa7fd2bfb8b0ffc
Danyil Marchuk <zemero58@gmail.com>
2023-02-15 19:38:02 +0100

    initial commit

--- NeumorphicCalculator/ContentView.swift
+++ NeumorphicCalculator/ContentView.swift
@@ -6,18 +6,34 @@
 //
 
 import SwiftUI
+import Neumorphic
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        let mainColor = Color.Neumorphic.main
+        ZStack {
+            mainColor.edgesIgnoringSafeArea(.all)
+        HStack {
+            Spacer()
+            Button(action: {}) {
+                Text(".none").fontWeight(.bold)
+            }.softButtonStyle(Capsule(), pressedEffect: .hard)
+            Spacer()
+            Button(action: {}) {
+                Text(".flat").fontWeight(.bold)
+            }.softButtonStyle(Capsule(), pressedEffect: .hard)
+            Spacer()
+            Button(action: {}) {
+                Text(".hard").fontWeight(.bold)
+            }.softButtonStyle(Capsule(), pressedEffect: .hard)
+            Spacer()
         }
+    }
         .padding()
     }
 }
+
+
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
