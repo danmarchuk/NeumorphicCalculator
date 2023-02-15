d834dfc6bace8658842e7a9679cd530847ef0e4b
Danyil Marchuk <zemero58@gmail.com>
2023-02-15 19:05:07 +0100

    Initial Commit

---
+++ NeumorphicCalculator/NeumorphicCalculatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  NeumorphicCalculatorApp.swift
+//  NeumorphicCalculator
+//
+//  Created by Данік on 15/02/2023.
+//
+
+import SwiftUI
+
+@main
+struct NeumorphicCalculatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
