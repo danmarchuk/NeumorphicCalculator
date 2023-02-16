ed076b53f815a70538925ca4a90dea871655f294
Danyil Marchuk <zemero58@gmail.com>
2023-02-16 17:31:57 +0100

    UI changes

--- NeumorphicCalculator/ContentView.swift
+++ NeumorphicCalculator/ContentView.swift
@@ -12,6 +12,7 @@ struct ContentView: View {
     
     
     let buttons = [
+        ["AC", "+/-", "%", "÷"],
         ["7", "8", "9", "x"],
         ["4", "5", "6", "-"],
         ["1", "2", "3", "+"],
@@ -22,29 +23,39 @@ struct ContentView: View {
     var body: some View {
         let mainColor = Color.Neumorphic.main
         ZStack (alignment: .bottom){
-            
-
             mainColor.edgesIgnoringSafeArea(.all)
-            VStack{
+            VStack(spacing: 20){
                 ZStack {
-                    RoundedRectangle(cornerRadius: 20).fill(Color.Neumorphic.main).softOuterShadow()
-                    HStack {Spacer()
-                        Text("42").foregroundColor(.black).font(.system(size: 64))}.padding()
-                    
+                    RoundedRectangle(cornerRadius: 20).fill(mainColor).softOuterShadow()
+                    VStack {
+                        Spacer()
+                        HStack{
+                            Spacer()
+                            Text("42")
+                            .foregroundColor(.black)
+                            .font(.system(size: 64))}.padding()}
                 }.padding()
-                    ForEach(buttons, id: \.self) { row in
-                        HStack {
-                            ForEach(row, id: \.self) { button in
-                                Button(action: {}) {
-                                    Text(button).fontWeight(.bold).frame(width: 50, height: 50)
-                                }.softButtonStyle(Capsule(), pressedEffect: .hard)
-                            }
+                
+                ForEach(buttons, id: \.self) { row in
+                    HStack(spacing: 15) {
+                        ForEach(row, id: \.self) { button in
+                            Button(action: {}) {
+                                Text(button)
+                                    .fontWeight(.bold)
+                                    .foregroundColor(.black)
+                                    .frame(width: self.buttonWidth(), height: self.buttonWidth()).font(.system(size: 30))
+                            }.softButtonStyle(RoundedRectangle(cornerRadius: 20), pressedEffect: .hard)
                         }
                     }
-
+                }
                 
-            }
+            }.padding(.bottom)
+        }
     }
+    
+    
+    func buttonWidth() -> CGFloat {
+        return (UIScreen.main.bounds.width - 5 * 12) / 7
     }
 }
 
